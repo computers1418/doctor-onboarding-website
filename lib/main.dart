3628291f0af641a45866b7fb00dca51910f02250
GondaliyaKishan <88085512+GondaliyaKishan@users.noreply.github.com>
2024-08-05 19:07:07 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
+
 import 'package:doctor_app/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() async{
-  
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setPreferredOrientations([
@@ -11,10 +12,20 @@ void main() async{
     DeviceOrientation.portraitDown,
   ]);
 
-    SystemChrome.setSystemUIOverlayStyle( SystemUiOverlayStyle(
-      statusBarColor: Colors.white.withOpacity(0.6),
-      statusBarIconBrightness: Brightness.dark,
-    ));
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.white.withOpacity(0.6),
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  HttpOverrides.global = MyHttpOverrides();
 
   runApp(const App());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
